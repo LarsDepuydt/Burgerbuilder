9fcf757fa9765e3a89e1cfbd3b7b45697dd47ced
Lars Depuydt <lars.depuydt@gmail.com>
2019-07-17 20:09:35 +0200

    safe file (without urls)

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -48,9 +48,9 @@ export const auth = (email, password, isSignup) => {
             password: password,
             returnSecureToken: true
         };
-        let url = 'https://www.googleapis.com/identitytoolkit/v3/relyingparty/signupNewUser?key=AIzaSyAOGhAGvxa5POLfEFod9qFovqA9IAwvbOE'
+        let url = '';
         if (!isSignup) {
-            url = 'https://www.googleapis.com/identitytoolkit/v3/relyingparty/verifyPassword?key=AIzaSyAOGhAGvxa5POLfEFod9qFovqA9IAwvbOE'
+            url = '';
         }
         axios.post(url, authData)
             .then(response => {
